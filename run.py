f784c3482755dc3edfa38122f83fb8fd978239f1
ac-wolf <ac@sem-wolf.org>
2020-09-14 01:52:02 +0300

    Changes to the get_input function

---
+++ run.py
@@ -0,0 +1,30 @@
+from utils import clear_string
+from autoComplate import get_best_k_completions, print_best_k_completions
+import datetime
+
+
+def get_input():
+    string = ""
+    new_input = input("")
+    while new_input == "" or new_input == " ":
+        new_input = input(">>>")
+
+    while new_input == "" or new_input[len(new_input)-1] != '#':
+        string += new_input
+        yield clear_string(string)
+        new_input = input(string)
+
+
+def run():
+    while 1:
+        print(">>>", end=" ")
+        for string in get_input():
+            print(datetime.datetime.now())
+            the_best = get_best_k_completions(string)
+            print_best_k_completions(the_best)
+            print(datetime.datetime.now())
+            print(">>>", end=" ")
+
+
+
+
